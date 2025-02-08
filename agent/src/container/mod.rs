58f97393e27a255260c1570bf898866784c4a5fc
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2025-02-08 09:34:49 +0100

    feat(container):remove

--- agent/src/container/mod.rs
+++ agent/src/container/mod.rs
@@ -1,10 +1,17 @@
-use containerd_client::services::v1::{container::Runtime, Container as ContainerD, CreateContainerRequest};
-use image::{Image, Pull};
-
 use crate::client_container::ClientContainer;
+use containerd_client::{
+    services::v1::{
+        container::Runtime, Container as ContainerD, CreateContainerRequest, DeleteContainerRequest,
+    },
+    with_namespace,
+};
+use image::{Image, Pull};
 use std::error::Error;
+use tonic::Request;
 pub mod image;
 pub mod old;
+
+const NAMESPACE: &str = "default";
 
 pub struct Container {
     client: ClientContainer,
@@ -15,7 +22,7 @@ pub struct Container {
 pub trait Manager {
     async fn launch(&self) -> Result<(), Box<dyn Error>>;
     async fn stop(&self) -> Result<(), Box<dyn Error>>;
-    async fn remove(&self) -> Result<(), Box<dyn Error>>;
+    async fn remove(self) -> Result<(), Box<dyn Error>>;
     async fn exec(&self) -> Result<(), Box<dyn Error>>;
 }
 
@@ -33,37 +40,45 @@ impl Builder for Container {
     ) -> Result<Container, Box<dyn Error>> {
         let image = Image::new(image_name, client.clone());
         image.clone().pull().await?;
-        Ok(Container { client, image, id: "".to_string() })
+        Ok(Container {
+            client,
+            image,
+            id: "".to_string(),
+        })
     }
 }
 
 impl Manager for Container {
     async fn launch(&self) -> Result<(), Box<dyn Error>> {
-            // let spec = include_str!("container_spec.json");
-            // let spec = spec
-            //         .to_string()
-            //         .replace("$ROOTFS", rootfs)
-            //         .replace("$OUTPUT", output);
-            
-            //     let spec = Any {
-            //         type_url: "types.containerd.io/opencontainers/runtime-spec/1/Spec".to_string(),
-            //         value: spec.into_bytes(),
-            //     };
-        let container = ContainerD::
-        let request = CreateContainerRequest {
-            container: Some(container),
-        };
-        self.client.containers.create(request);
-        Ok(())
-        
+        // let spec = include_str!("container_spec.json");
+        // let spec = spec
+        //         .to_string()
+        //         .replace("$ROOTFS", rootfs)
+        //         .replace("$OUTPUT", output);
+
+        //     let spec = Any {
+        //         type_url: "types.containerd.io/opencontainers/runtime-spec/1/Spec".to_string(),
+        //         value: spec.into_bytes(),
+        //     };
+        // let container = ContainerD::
+        // let request = CreateContainerRequest {
+        //     container: Some(container),
+        // };
+        // self.client.containers.create(request);
+        // Ok(())
+        unimplemented!()
     }
 
     async fn stop(&self) -> Result<(), Box<dyn Error>> {
         unimplemented!()
     }
 
-    async fn remove(&self) -> Result<(), Box<dyn Error>> {
-        unimplemented!()
+    async fn remove(mut self) -> Result<(), Box<dyn Error>> {
+        let req = DeleteContainerRequest { id: self.id };
+        let req = with_namespace!(req, NAMESPACE);
+
+        let _resp = self.client.containers.delete(req).await?;
+        Ok(())
     }
 
     async fn exec(&self) -> Result<(), Box<dyn Error>> {
